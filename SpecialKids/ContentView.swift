660749142c3a69995d14d9271b73de53a25cdd30
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2024-11-20 18:04:20 +0300

    Initial Commit

---
+++ SpecialKids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpecialKids
+//
+//  Created by Mehmet Ali Bunsuz on 20.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpecialKids/SpecialKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialKidsApp.swift
+//  SpecialKids
+//
+//  Created by Mehmet Ali Bunsuz on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpecialKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
